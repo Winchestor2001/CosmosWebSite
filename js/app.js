ee563bd648ce5a39e7d551e27f28e5184030c920
Winchestor2001 <sbexruz2001@gmail.com>
2021-08-27 17:09:42 +0500

    Cosmos-Web

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.slider__box').slick({
+        prevArrow:'<img class="slider__arrow slider__arrow-left" src="img/arrow-left.svg" alt="arrow-left">',
+        nextArrow:'<img class="slider__arrow slider__arrow-right" src="img/arrow-right.svg" alt="arrow-right">',
+    });
+
+
+    $('.menu-btn').click(function(){
+        $('.menu__list').toggleClass('active')
+    });
+
+
+
+});
