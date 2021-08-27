966e16782a006a3556236fa3755c4784035ba867
Winchestor <75200506+Winchestor2001@users.noreply.github.com>
2021-08-27 17:54:39 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-$(function(){
-    $('.slider__box').slick({
-        prevArrow:'<img class="slider__arrow slider__arrow-left" src="img/arrow-left.svg" alt="arrow-left">',
-        nextArrow:'<img class="slider__arrow slider__arrow-right" src="img/arrow-right.svg" alt="arrow-right">',
-    });
+$(function () {
+  $(".slider__box").slick({
+    prevArrow:
+      '<img class="slider__arrow slider__arrow-left" src="img/arrow-left.svg" alt="arrow-left">',
+    nextArrow:
+      '<img class="slider__arrow slider__arrow-right" src="img/arrow-right.svg" alt="arrow-right">',
+  });
 
-
-    $('.menu-btn').click(function(){
-        $('.menu__list').toggleClass('active')
-    });
-
-
-
+  $(".menu-btn").click(function () {
+    $(".menu__list").toggleClass("active");
+  });
 });
+
+
+window.onload = function(){
+    let preloader = $('#preloader__time');
+    preloader.css({display: 'none'})
+    // preloader.css({opacity: '0'})
+}
+
+
